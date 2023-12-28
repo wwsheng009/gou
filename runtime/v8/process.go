f5a685b05cc6f931a638dad03d033203111cc6b7
wwsheng009 <vincentwwsheng@gmail.com>
2023-12-29 00:19:10 +0800

    remove the vm stat

--- runtime/v8/process.go
+++ runtime/v8/process.go
@@ -8,8 +8,8 @@ import (
 func init() {
 	process.Register("scripts", processScripts)
 	process.Register("studio", processStudio)
-	process.Register("runtime.v8.stats", processV8IsoStats)
-	process.Register("runtime.v8.statTotal", processV8TotalStat)
+	// process.Register("runtime.v8.stats", processV8IsoStats)
+	// process.Register("runtime.v8.statTotal", processV8TotalStat)
 	process.Register("runtime.v8.option", processV8Option)
 	process.Register("runtime.v8.restart", processV8Restart)
 	process.Register("runtime.v8.stop", processV8Stop)
@@ -38,41 +38,6 @@ func processStudio(process *process.Process) interface{} {
 	}
 	return script.Exec(process)
 
-}
-func processV8TotalStat(process *process.Process) interface{} {
-	total := HeapTotalStatistics{}
-	isolates.Data.Range(func(key, value any) bool {
-		if iso, ok := key.(*Isolate); ok {
-			stat := iso.HeapStat()
-			total.TotalHeapSize += stat.TotalHeapSize
-			total.TotalHeapSizeExecutable += stat.TotalHeapSizeExecutable
-			total.TotalPhysicalSize += stat.TotalPhysicalSize
-			total.TotalAvailableSize += stat.TotalAvailableSize
-			total.UsedHeapSize += stat.UsedHeapSize
-			total.HeapSizeLimit += stat.HeapSizeLimit
-			total.MallocedMemory += stat.MallocedMemory
-			total.ExternalMemory += stat.ExternalMemory
-			total.PeakMallocedMemory += stat.PeakMallocedMemory
-			total.NumberOfNativeContexts += stat.NumberOfNativeContexts
-			total.NumberOfDetachedContexts += stat.NumberOfDetachedContexts
-			total.Count += 1
-		}
-
-		return true
-	})
-	total.Length = uint64(isolates.Len)
-
-	return total
-}
-func processV8IsoStats(process *process.Process) interface{} {
-	stats := make([]HeapStatistics, 0)
-	isolates.Data.Range(func(key, value any) bool {
-		if iso, ok := key.(*Isolate); ok {
-			stats = append(stats, iso.HeapStat())
-		}
-		return true
-	})
-	return stats
 }
 
 func processV8Option(process *process.Process) interface{} {
